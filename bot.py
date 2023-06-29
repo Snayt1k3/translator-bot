35f5f395b510c0c17a0fcb9e3aaa223e1bc9a82f
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-06-29 14:11:50 +0300

    add EchoTranslate

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+
 API_TOKEN = os.environ.get("TOKEN")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(message)s',  # msg form
                     handlers=[
-                        logging.FileHandler('app.log'),  # write in file
                         logging.StreamHandler()  # console
                     ])
 
--- handlers/main.py
+++ handlers/main.py
@@ -1,9 +1,14 @@
+import os
+
 from aiogram import Dispatcher, types
+import deepl
 
 
 async def EchoTranslate(msg: types.Message):
-    pass
+    translator = deepl.Translator(os.getenv('DEEPL_KEY'))
+    result = translator.translate_text(msg.text, target_lang="RU")
+    await msg.answer(result.text)
 
 
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(EchoTranslate)
+    dp.register_message_handler(EchoTranslate, lambda msg: '/' not in msg.text)
