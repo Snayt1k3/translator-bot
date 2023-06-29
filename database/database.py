b9239d527e33de737a4259560c78b3de33199df0
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-06-29 14:35:36 +0300

    add save msg for history

--- database/database.py
+++ database/database.py
@@ -1,5 +1,19 @@
-import pymongo
+import motor.motor_asyncio
+import os
 
 
 class DataBase:
-    pass
+    __database = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGO_URI_DEV'))
+    __current_database = __database['translator-bot']
+
+    @classmethod
+    async def InsertOne(cls, coll, data=None):
+        if data is None:
+            return
+
+        db = cls.__current_database[coll]
+        await db.insert_one(data)
+
+
+
+
--- handlers/main.py
+++ handlers/main.py
@@ -1,5 +1,5 @@
 import os
-
+from database.database import DataBase
 from aiogram import Dispatcher, types
 import deepl
 
@@ -7,6 +7,16 @@ import deepl
 async def EchoTranslate(msg: types.Message):
     translator = deepl.Translator(os.getenv('DEEPL_KEY'))
     result = translator.translate_text(msg.text, target_lang="RU")
+
+    # save msg for view user msg history
+    await DataBase.InsertOne(
+        'message_history',
+        {
+            'user_id': msg.from_user.id,
+            'msg': msg.text,
+            'translated_msg': result.text
+        })
+
     await msg.answer(result.text)
 
 
